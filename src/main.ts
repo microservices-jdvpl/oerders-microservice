a25f935ef83be78f0abe4738275fa0e651b2a200
jdsuarez <jdsuarez@fgs.co>
2024-11-17 13:24:56 -0500

    runnig orders-Ms

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main orders-MS');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.PORT);
+  logger.log(`Ms is running on ${await app.getUrl()}`);
 }
 bootstrap();
