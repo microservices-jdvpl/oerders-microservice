df8b4bf71b6ab86dff0ec9acc3913b9deed21d00
jdsuarez <jdsuarez@fgs.co>
2024-11-17 13:54:57 -0500

    feat: implement orders microservice with create, find, and update functionalities

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main orders-MS');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.PORT);
-  logger.log(`Ms is running on ${await app.getUrl()}`);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
+  await app.listen();
+  logger.log(`Ms is running on ${envs.PORT}`);
 }
 bootstrap();
